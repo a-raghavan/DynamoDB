8b2d361cd53fd076e61bee8736915c85b0abc5de
a-raghavan <akshayraghavan@gmail.com>
2023-04-23 18:10:01 -0500

    Changes in leader election

--- rsm.py
+++ rsm.py
@@ -1,39 +1,25 @@
 from kazoo.client import KazooClient
+from kazoo.exceptions import NodeExistsError
+from kazoo.protocol.states import EventType
 import signal
 import sys
 import time
 import random
 
-def signal_handler(sig, frame):
-    print('You pressed Ctrl+C!')
-    sys.exit(0)
-
-def getLeaderFile(guids) -> str:
-    leader_guid = 10000000000
-    leader_file =""
-    for c in guids:
-        t = int(c.split('_')[1])
-        if leader_guid > t:
-            leader_guid = t
-            leader_file = c
-    return leader_file
-
 class LeaderElection:
-    def __init__(self, id, zk) -> None:
+    def __init__(self, id, rsm) -> None:
         self.myID = id
         self.currLeader = ""
-        self.zk = zk
-
-    def contest(self) -> None:
-        self.zk.ensure_path("/election")
-        self.zk.create("/election/guid-n_", value=bytes(sys.argv[1], encoding='utf8'), ephemeral=True, sequence=True)
-        self.findLeader()
+        self.rsm = rsm
     
-    def findLeader(self) -> None:
-        children = self.zk.get_children("/election", watch=None, include_data=False)
-        leader_file = getLeaderFile(children)
-        data = self.zk.get("/election/"+leader_file)
-        self.currLeader = data[0].decode()
+    def contest(self) -> None:
+        self.rsm.zk.ensure_path("/election")
+        try:
+            self.rsm.zk.create("/election/leader", value=bytes(sys.argv[1], encoding='utf8'), ephemeral=True)
+            self.currLeader = self.myID
+        except NodeExistsError:
+            data = self.rsm.zk.get("/election/leader", watch=self.rsm.watchLeaderFile)
+            self.currLeader = data[0].decode()
     
     def leader(self) -> bool:
         return self.myID == self.currLeader
@@ -43,34 +29,47 @@ class ReplicatedStateMachine:
         self.zk = KazooClient(hosts='127.0.0.1:2181')
         self.zk.start()
         self.isFollower = True
-        self.electionModule = LeaderElection(id, self.zk)
+        self.electionModule = LeaderElection(id, self)
+        signal.signal(signal.SIGINT, self.signal_handler)
     
     def __del__(self):
         self.zk.stop()
+    
+    def watchLeaderFile(self, event):
+        if event.type == EventType.DELETED:
+            self.isFollower = False
         
     def run(self):
-        self.electionModule.contest()
-        if self.electionModule.leader():
-            self.isFollower = False
-            self.leader_function()
-        else:
-            self.follower_function()
+        while True:
+            self.electionModule.contest()
+            if self.electionModule.leader():
+                self.isFollower = False
+                self.leader_function()
+            else:
+                self.isFollower = True
+                self.follower_function()
 
     def leader_function(self):
         print("I'm the king of the world ", sys.argv[1])
+        signal.pause()
+    
+    def signal_handler(self, sig, frame):
+        print('You pressed Ctrl+C!')
+        self.zk.stop()
+        sys.exit(0)
     
     def follower_function(self):
         print("I hate following others ", sys.argv[1])
+        while self.isFollower:
+            time.sleep(1)
 
 if __name__ == "__main__":
-    signal.signal(signal.SIGINT, signal_handler)
 
     rsm = ReplicatedStateMachine(sys.argv[1])
 
     time.sleep(random.random()+1) 
 
     rsm.run()
-    #signal.pause()
     
     time.sleep(3)
 
