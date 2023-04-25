5fd40519047580c25977db46444ff65e6fb2c14d
a-raghavan <akshayraghavan@gmail.com>
2023-04-25 18:45:51 -0500

    Comments and removed prints

--- rsm.py
+++ rsm.py
@@ -17,6 +17,14 @@ import database_pb2
 import database_pb2_grpc
 
 def othernodes(nodes, port):
+    '''
+    Helper function to extract peers by eliminating my nodeport
+    Args:
+        nodes: set of all nodes in the network
+        port: my port
+    returns:
+        list of peers
+    '''
     import socket
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     s.connect(("8.8.8.8", 80))
@@ -26,6 +34,9 @@ def othernodes(nodes, port):
     return ret
 
 class Leader(database_pb2_grpc.DatabaseServicer):
+    '''
+    Leader class that listens for input get/put RPC requests from the upper layer
+    '''
     def __init__(self, rsm):
         self.rsm = rsm 
     
@@ -45,6 +56,9 @@ class Leader(database_pb2_grpc.DatabaseServicer):
         server.wait_for_termination()
 
 class Follower(rsm_pb2_grpc.RSMServicer):
+    '''
+    Follower class that listens to AppendEntries to replicate distributed log
+    '''
     def __init__(self, rsm):
         self.rsm = rsm
         _thread = threading.Thread(target= self.__grpcServerThread)
@@ -61,9 +75,10 @@ class Follower(rsm_pb2_grpc.RSMServicer):
         self.server.stop()
 
     def AppendEntries(self, request, context):
+        # send my log length if master tries to append past my log length
         if len(self.rsm.log) != request.index:
             return rsm_pb2.AppendEntriesResponse(success=False, index=len(self.rsm.log))
-        print("AppendEntries - ", request.index, request.entries)
+        # append all entries in the request to my replicated log and return success
         for e in request.entries:
             self.rsm.log.append(e)
             self.rsm.db.Put(bytearray(e.key, 'utf-8'), bytearray(e.value, 'utf-8'))
@@ -72,15 +87,20 @@ class Follower(rsm_pb2_grpc.RSMServicer):
         return rsm_pb2.AppendEntriesResponse(success=True, index=len(self.rsm.log))
 
     def GetCommitIndex(self, request, context):
+        # return my log length to peer. Used in leader election
         return rsm_pb2.GetCommitIndexResponse(commitindex=len(self.rsm.log))
 
 class LeaderElection:
+    ''' 
+    Leader Election module for the Replicated state machine
+    '''
     def __init__(self, id, rsm) -> None:
         self.myID = id
         self.currLeader = ""
         self.rsm = rsm
     
     def contest(self) -> None:
+        # Contest in election
         self.rsm.zk.ensure_path("/election")
 
         peerNumProcessedEntries = -1
@@ -112,32 +132,48 @@ class LeaderElection:
                 data = self.rsm.zk.get("/election/leader", watch=self.rsm.watchLeaderFile)
                 self.currLeader = data[0].decode()
         else:
-            # allow peer to become the leader
+            # allow peer to become the leader as it has a longer log
             self.rsm.zk.exists("/election/leader", watch=self.rsm.watchLeaderFile)
             self.currLeader = peerid
     
     def leader(self) -> bool:
+        # check if I am the leader
         return self.myID == self.currLeader
 
 class ReplicatedStateMachine:
+    '''
+    The replicated state machine
+    '''
     def getpeer(self, port):
+        '''
+        Helper function to get a peer when it's port is given
+        '''
         for np in self.peers:
             if port == np.split(':')[1]:
                 return np
         return ""
 
     def setupDB(self, id):
+        '''
+        Helper method to set up levelDB
+        '''
         dbpath = './{}_db'.format(id)
-        from shutil import rmtree
-        rmtree(dbpath, ignore_errors=True)
+        #from shutil import rmtree
+        #rmtree(dbpath, ignore_errors=True)
         self.db = leveldb.LevelDB(dbpath)
 
     def persistLog(self, entry):
+        '''
+        Helper to persist log entry to disk
+        '''
         logpath = './{}_log'.format(self.port)
         with open(logpath, 'a') as file:
             file.write(entry.command + " " + entry.key + " " + entry.value+"\n")
     
     def retreivePersistedLog(self):
+        '''
+        Helper function to fetch log entries to memory upon start up
+        '''
         logpath = './{}_log'.format(self.port)
         try:
             with open(logpath, 'r') as file:
@@ -178,6 +214,9 @@ class ReplicatedStateMachine:
         self.zk.stop()
     
     def watchLeaderFile(self, event):
+        '''
+        Kazoo watcher to trigger election when leader dies
+        '''
         if event.type == EventType.DELETED:
             self.isFollower = False
         elif event.type == EventType.CREATED:
@@ -186,6 +225,7 @@ class ReplicatedStateMachine:
 
     def run(self):
         while True:
+            # need followers to listen for GetCommitIndex RPC calls
             follower = Follower(self)
             self.electionModule.contest()
             del follower
@@ -198,6 +238,9 @@ class ReplicatedStateMachine:
                 self.follower_function()
 
     def leader_function(self):
+        '''
+        wait for incoming upper layer requests (GET/PUT)
+        '''
         print("I'm the king of the world ")
         leader = Leader(self)
         leader.serve()
@@ -208,6 +251,9 @@ class ReplicatedStateMachine:
         sys.exit(0)
     
     def follower_function(self):
+        '''
+        Wait for replication events from leader
+        '''
         print("I hate following others ")
         follower = Follower(self)
         while self.isFollower:
@@ -215,6 +261,9 @@ class ReplicatedStateMachine:
         del follower
     
     def __appendEntries(self, follower, currentry, idx):
+        '''
+        Thread that appends replicated log enttries to followers
+        '''
         with grpc.insecure_channel(follower) as channel:
             stub = rsm_pb2_grpc.RSMStub(channel)
             response = rsm_pb2.AppendEntriesResponse(success=False, index=0)
@@ -233,9 +282,16 @@ class ReplicatedStateMachine:
                 if response.success:
                     return
 
+                # send log entries from follower requested index
                 reqidx = response.index
                 entries = self.log[reqidx:]
                 
+                # do not miss current entry when sending log entries from requested index
+                # this condition distinguishes 2 cases:
+                # 1. Follower catching up after it restarted. 
+                #       currentry might have been replicated in another follower and added to self.log 
+                # 2. Follower catching up. Other follower is currently dead. 
+                #       Meaning catchup is requried and currentry must also be sent as it is commited to self.log after committing in the follower
                 if idx == len(self.log):
                     entries.append(currentry)
 
@@ -246,10 +302,12 @@ class ReplicatedStateMachine:
         currentry = rsm_pb2.LogEntry(command="PUT", key=key, value=value)
         idx = len(self.log)
 
+        # Submit jobs to append entries in followers
         f1 = self.replicateFollower1.submit(self.__appendEntries, follower=self.peers[0], currentry=currentry, idx=idx)
         f2 = self.replicateFollower2.submit(self.__appendEntries, follower=self.peers[1], currentry=currentry, idx=idx)
 
         while True:
+            # If either one is done, majority replication achieved. break
             if f1.done() or f2.done():
                 break
             time.sleep(0.1)
@@ -263,6 +321,8 @@ class ReplicatedStateMachine:
         return True
     
     def get(self, key):
+        # get from local db
+        # GETs from upper layer are sent only to the leader, strong consistency
         try:
             val = self.db.Get(bytearray(key, 'utf-8'))
         except Exception as e:
